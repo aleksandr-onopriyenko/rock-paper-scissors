72d40bbd74515e2f77cafd3c9e1b150f3f510b7b
aleksandr-onopriyenko <onopriyenkoalex@gmail.com>
2023-05-23 23:48:01 +0300

    feat: add template of Game in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import {
+  GameWrapper,
+  GameChoices,
+  GameProcess,
+  GameRules,
+} from "@/components/Game";
+import { Header } from "@/components/Header";
+import { ModeContext } from "@/context/modeContext";
+import { useGameProcess } from "@/hooks/useGameProcess";
+
 export const App = () => {
+  const { result, restart, handleMode, choices, activeClass, handleChoice } =
+    useGameProcess();
+
   return (
-    <div className="game-wrapper">
-      {/* <Header />
-      <Game /> */}
-    </div>
+    <ModeContext.Provider value={result}>
+      <Header onClick={handleMode} />
+      <GameWrapper>
+        <GameProcess
+          result={result}
+          restart={restart}
+          handleChoice={handleChoice}
+        />
+        <GameChoices
+          choices={choices}
+          mode={result.mode}
+          onChoice={handleChoice}
+          classActive={activeClass}
+          isChoices={!!result.yourChoice}
+        />
+      </GameWrapper>
+      <GameRules mode={result.mode} />
+    </ModeContext.Provider>
   );
 };
