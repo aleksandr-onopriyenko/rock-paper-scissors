5cd36992a6b9bb662fa3761318e3dd000ac0f0d2
aleksandr-onopriyenko <onopriyenkoalex@gmail.com>
2023-05-23 23:39:36 +0300

    feat: create Toggle component

---
+++ src/components/UI/Toggle/Toggle.tsx
@@ -0,0 +1,13 @@
+import { ComponentPropsWithoutRef, FC } from "react";
+
+import "./style.scss";
+
+export const Toggle: FC<
+  { isOff: boolean } & ComponentPropsWithoutRef<"button">
+> = ({ isOff, ...props }) => (
+  <button
+    {...props}
+    className={`toggle toggle--${isOff ? "off" : "on"}`}
+    type="button"
+  />
+);
