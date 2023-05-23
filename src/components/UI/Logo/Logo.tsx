37b40db814b390282b2a9d2b29c1a6119150d648
aleksandr-onopriyenko <onopriyenkoalex@gmail.com>
2023-05-23 23:38:17 +0300

    feat: create Logo component

--- src/components/UI/Logo/Logo.tsx
+++ src/components/UI/Logo/Logo.tsx
@@ -1,10 +1,18 @@
 import { FC } from "react";
 
+import { Mode } from "@/constants/initialGameState";
+import basic from "@/assets/images/logo.svg";
+import advance from "@/assets/images/logo-bonus.svg";
+
 import "./style.scss";
 
 interface Props {
   mode: "basic" | "advance";
 }
 export const Logo: FC<Props> = ({ mode }) => {
-  return <div className={`logo logo--${mode}`}></div>;
+  return (
+    <div className={`logo logo--${mode}`}>
+      <img src={mode === Mode.Basic ? basic : advance} />
+    </div>
+  );
 };
